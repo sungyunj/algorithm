0e2be8b79e8766a1263e6ccf239e215f554d60e8
성윤 <145305570+sungyunj@users.noreply.github.com>
2024-12-05 21:43:03 +0900

    Update 2005.swift

--- SWEA/2005.swift
+++ SWEA/2005.swift
@@ -16,14 +16,29 @@
 // [입력]
 
 // 가장 첫 줄에는 테스트 케이스의 개수 T가 주어지고, 그 아래로 각 테스트 케이스가 주어진다.
-
 // 각 테스트 케이스에는 N이 주어진다.
 
 
 // [출력]
 
 // 각 줄은 '#t'로 시작하고, 다음 줄부터 파스칼의 삼각형을 출력한다.
-
 // 삼각형 각 줄의 처음 숫자가 나오기 전까지의 빈 칸은 생략하고 숫자들 사이에는 한 칸의 빈칸을 출력한다.
-
 // (t는 테스트 케이스의 번호를 의미하며 1부터 시작한다.)
+
+import Foundation
+
+func pascal(_ n: Int) -> [[Int]] {
+    var triangle: [[Int]] = []
+    
+    for i in 0..<n {
+        var row = Array(repeating: 1, count: i + 1) // 각 줄은 1로 시작하고 끝남
+        for j in 1..<i {
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j] // 왼쪽 위와 오른쪽 위 숫자의 합 계산
+        }
+        triangle.append(row)
+    }
+    
+    return triangle
+}
+
+
