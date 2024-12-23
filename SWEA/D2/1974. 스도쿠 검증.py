9ac3d6971fd9eb875470f640a59092460573d904
성윤 <145305570+sungyunj@users.noreply.github.com>
2024-12-23 19:21:05 +0900

    feat: algorithm 문제 16 풀기

--- SWEA/D2/1974. 스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증.py
@@ -17,3 +17,31 @@
 # 테스트 케이스 t에 대한 결과는 “#t”을 찍고, 한 칸 띄고, 정답을 출력한다.
 # (t는 테스트 케이스의 번호를 의미하며 1부터 시작한다.)
 
+T = int(input())
+for test_case in range(1, T + 1):
+    arr = [list(map(int, input().split())) for _ in range(9)]
+
+    def checkSudoku():
+        # 조건1: 3x3 박스 검사
+        for r in range(0, 9, 3):  # 3칸씩 건너뜀
+            for c in range(0, 9, 3):
+                numbers_box = []
+                for i in range(3):
+                    for j in range(3):
+                        numbers_box.append(arr[r + i][c + j])
+                if sorted(numbers_box) != list(range(1, 10)):  # 1~9 포함 확인
+                    return 0
+
+        # 조건2: 가로와 세로 검사
+        for i in range(9):
+            row = []
+            column = []
+            for j in range(9):
+                row.append(arr[i][j])      # i번째 행
+                column.append(arr[j][i])  # i번째 열
+            if sorted(row) != list(range(1, 10)) or sorted(column) != list(range(1, 10)):
+                return 0
+
+        return 1
+
+    print(f"#{test_case} {checkSudoku()}")
