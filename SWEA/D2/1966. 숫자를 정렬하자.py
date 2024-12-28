3af19458d27c3b690ab315e54ff680270bcdd895
성윤 <145305570+sungyunj@users.noreply.github.com>
2024-12-28 21:02:12 +0900

    feat: algorithm 문제 18 풀기

--- SWEA/D2/1966. 숫자를 정렬하자.py
+++ SWEA/D2/1966. 숫자를 정렬하자.py
@@ -12,3 +12,13 @@
 # [출력]
 # 출력의 각 줄은 '#t'로 시작하고, 공백을 한 칸 둔 다음 정답을 출력한다.
 # (t는 테스트 케이스의 번호를 의미하며 1부터 시작한다.)
+
+T = int(input())
+
+for t in range(1, T + 1):
+    N = int(input())
+    numbers = list(map(int, input().split()))
+
+    numbers.sort()
+
+    print(f"#{t} {' '.join(map(str, numbers))}")
