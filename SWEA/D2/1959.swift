1697f02188226afd79fe31dd5ff5fc878b252f32
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-01-02 20:37:46 +0900

    feat: 1959 swift로 풀어보기

--- SWEA/D2/1959.swift
+++ SWEA/D2/1959.swift
@@ -18,3 +18,40 @@
 // [출력]
 // 출력의 각 줄은 '#t'로 시작하고, 공백을 한 칸 둔 다음 정답을 출력한다.
 // (t는 테스트 케이스의 번호를 의미하며 1부터 시작한다.)
+
+
+import Foundation
+
+if let T = Int(readLine()!) {
+    for t in 1...T {
+
+        let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+        var N = input[0]
+        var M = input[1]
+        
+        let A = readLine()!.split(separator: " ").compactMap { Int($0) }
+        let B = readLine()!.split(separator: " ").compactMap { Int($0) }
+
+        var shorter = A
+        var longer = B
+
+        // A B 길이를 비교해서 스왑
+        if N > M {
+            shorter = B
+            longer = A
+            swap(&N, &M)
+        }
+
+        var maxSum = Int.min
+
+        // 짧은 배열 긴 배열에 맞춰 이동
+        for i in 0...(M - N) {
+            var tempSum = 0
+            for j in 0..<N {
+                tempSum += shorter[j] * longer[i + j]
+            }
+            maxSum = max(maxSum, tempSum)
+
+        print("#\(t) \(maxSum)")
+    }
+}
