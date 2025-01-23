30bd733b83f948e8c395c6b1be2c167b8ee65cee
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-01-23 21:35:02 +0900

    feat: algorithm 문제 30 풀기

--- SWEA/D1/2071. 평균값 구하기.py
+++ SWEA/D1/2071. 평균값 구하기.py
@@ -12,3 +12,15 @@
 # 출력의 각 줄은 '#t'로 시작하고, 공백을 한 칸 둔 다음 정답을 출력한다.
 # (t는 테스트 케이스의 번호를 의미하며 1부터 시작한다.)
 
+T = int(input())
+
+for i in range(T):
+
+    tenNums = list(map(int, input().split()))
+    answer = 0
+    
+    for nums in tenNums:
+        answer += nums
+        
+    answer /= 10
+    print("#{} {}".format(i+1, round(answer)))
