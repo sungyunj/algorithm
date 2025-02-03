c73394591f8d9f855c81f05f48adb283a048a490
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-02-03 18:36:24 +0900

    feat: algorithm 문제 34 풀기

--- SWEA/D1/2072. 홀수만 더하기.py
+++ SWEA/D1/2072. 홀수만 더하기.py
@@ -14,3 +14,21 @@
 # 출력의 각 줄은 '#t'로 시작하고, 공백을 한 칸 둔 다음 정답을 출력한다.
 # (t는 테스트 케이스의 번호를 의미하며 1부터 시작한다.)
 
+T = int(input())
+
+results = []
+
+for i in range(1, T+1):
+    
+    odd_sum = 0
+    a = list(map(int,input().split()))
+
+    for j in range(len(a)):
+
+        if a[j] % 2 == 1:
+            odd_sum += a[j]
+    
+    results.append(f"#{i} {odd_sum}")
+
+for result in results:
+    print(result)
