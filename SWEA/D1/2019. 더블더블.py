024556437399a874d32bb070e22e6d432d654f13
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-03-03 14:46:42 +0900

    feat: algorithm 문제 47 풀기

--- SWEA/D1/2019. 더블더블.py
+++ SWEA/D1/2019. 더블더블.py
@@ -1,3 +1,13 @@
 # 1부터 주어진 횟수까지 2를 곱한 값(들)을 출력하시오.
 
 # 주어질 숫자는 30을 넘지 않는다.
+
+
+T = int(input())
+result = 1
+
+if T < 30:
+    for i in range(1, T+1):
+
+        print(result, end = ' ')
+        result *= 2
