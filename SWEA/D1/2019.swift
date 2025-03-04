a5751f92004051a636ee79e193fc0d53a54dffae
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-03-04 21:22:15 +0900

    feat: 2019 swift로 풀어보기

--- SWEA/D1/2019.swift
+++ SWEA/D1/2019.swift
@@ -1,3 +1,23 @@
 // 1부터 주어진 횟수까지 2를 곱한 값(들)을 출력하시오.
 
 // 주어질 숫자는 30을 넘지 않는다.
+
+
+if let input = readLine(), let T = Int(input) {
+    
+    var result = 1
+
+    if T < 30 {
+        for i in 1...T {
+
+            print(result, terminator:" ")
+            result *= 2
+        }
+    }
+}
+
+
+
+
+
+
