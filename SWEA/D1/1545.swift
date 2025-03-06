c956059f037b5cac48cb17504cb275f3ddbedcd0
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-03-06 21:57:32 +0900

    feat: 1545 swift로 풀어보기

--- SWEA/D1/1545.swift
+++ SWEA/D1/1545.swift
@@ -2,3 +2,14 @@
 
 // 아래는 입력된 숫자가 N일 때 거꾸로 출력하는 예시입니다.
 // N N-1 N-2 ... 0
+
+
+
+if let input = readLine(), let T = Int(input) {
+
+    for i in stride(from: T, through: 0, by: -1) {
+        print(i, terminator:" ")
+    }
+    print()
+}
+
