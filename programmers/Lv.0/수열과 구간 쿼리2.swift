f38ea4f127de6f639de8335fa45d5a66bf1ad7d5
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-03-16 19:19:49 +0900

    feat: algorithm 문제 53 swift로 풀기

--- programmers/Lv.0/수열과 구간 쿼리2.swift
+++ programmers/Lv.0/수열과 구간 쿼리2.swift
@@ -33,3 +33,18 @@
 // 따라서 [3, 4, -1]을 return 합니다.
 
 
+
+import Foundation
+
+func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
+
+    var result: [Int] = []
+
+    for query in queries {
+        
+        let temp = arr[query[0]...query[1]].filter { $0 > query[2] }
+        result.append(temp.min() ?? -1)
+    }
+
+    return result
+}
