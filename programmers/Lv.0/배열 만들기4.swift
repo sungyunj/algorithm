d38e0c162ec02ba74bca6ef267dab2ae0abb90fe
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-03-26 20:26:05 +0900

    feat: algorithm 문제 58 swift로 풀기

--- programmers/Lv.0/배열 만들기4.swift
+++ programmers/Lv.0/배열 만들기4.swift
@@ -37,3 +37,29 @@
 // 4	3	    [1, 2]
 // -	-	    [1, 2, 3]
 // 따라서 [1, 2, 3]을 return 합니다.
+
+
+
+import Foundation
+
+func solution(_ arr: [Int]) -> [Int] {
+    var stk = [Int]()
+    var i = 0
+
+    while i < arr.count {
+
+        if stk.isEmpty {
+            stk.append(arr[i])
+            i += 1
+        } 
+        else if stk.last! < arr[i] {
+            stk.append(arr[i])
+            i += 1
+        } 
+        else {
+            stk.removeLast()
+        }
+    }
+
+    return stk
+}
