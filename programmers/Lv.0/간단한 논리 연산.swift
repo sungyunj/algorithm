a8b4106d9c4191fdc1bbbeb4438f39978783a171
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-03-28 21:41:02 +0900

    feat: algorithm 문제 59 swift로 풀기

--- programmers/Lv.0/간단한 논리 연산.swift
+++ programmers/Lv.0/간단한 논리 연산.swift
@@ -35,3 +35,12 @@
 // T	F	  T	      F
 // F	T	  T	      F
 //      F	  F	      F
+
+
+
+import Foundation
+
+func solution(_ x1:Bool, _ x2:Bool, _ x3:Bool, _x4:Bool) -> Bool {
+
+    return (x1 || x2) && (x3 || x4)
+}
