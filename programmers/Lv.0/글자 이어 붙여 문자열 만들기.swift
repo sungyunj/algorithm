0bc34d1591bec94aac85d36dfaae23cd941879b6
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-04-01 19:12:22 +0900

    feat: algorithm 문제 61 swift로 풀기

--- programmers/Lv.0/글자 이어 붙여 문자열 만들기.swift
+++ programmers/Lv.0/글자 이어 붙여 문자열 만들기.swift
@@ -36,4 +36,14 @@
 
 
 
+import Foundation
 
+func solution(_ my_string: String, _ index_list: [Int]) -> String {
+
+    var answer = ""
+    
+    for i in index_list {
+        answer.append(my_string[my_string.index(my_string.startIndex, offsetBy: i)])
+    }
+    return answer
+}
