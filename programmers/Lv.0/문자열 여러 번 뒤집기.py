9ed7be80a1b396cb8c5216e217f1d9c341edd36f
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-04-04 20:18:04 +0900

    feat: algorithm 문제 63 풀기

--- programmers/Lv.0/문자열 여러 번 뒤집기.py
+++ programmers/Lv.0/문자열 여러 번 뒤집기.py
@@ -34,3 +34,15 @@
 # [6, 10]	    "programmers"
 
 # 따라서 "programmers"를 return 합니다.
+
+
+
+def solution(my_string, queries):
+
+    my_list = list(my_string)
+
+    for s, e in queries:
+        
+        my_list[s:e+1] = my_list[s:e+1][::-1]
+    
+    return ''.join(my_list)
