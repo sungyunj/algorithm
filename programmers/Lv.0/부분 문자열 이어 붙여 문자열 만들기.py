8bc220d4473e496479a74c4244bad8459b33d4be
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-04-08 20:38:38 +0900

    feat: algorithm 문제 65 풀기

--- programmers/Lv.0/부분 문자열 이어 붙여 문자열 만들기.py
+++ programmers/Lv.0/부분 문자열 이어 붙여 문자열 만들기.py
@@ -36,3 +36,13 @@
 
 
 
+
+def solution(my_strings, parts):
+    
+    answer = ''
+
+    for i in range(len(my_strings)):
+        s, e = parts[i]
+        answer += my_strings[i][s:e+1]
+
+    return answer
