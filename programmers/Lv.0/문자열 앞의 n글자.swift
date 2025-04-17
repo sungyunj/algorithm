a424dd8a2f1d63fc18666e80f057b26f3ae35fb5
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-04-17 23:10:22 +0900

    feat: algorithm 문제 69 swift로 풀기

--- programmers/Lv.0/문자열 앞의 n글자.swift
+++ programmers/Lv.0/문자열 앞의 n글자.swift
@@ -30,3 +30,13 @@
 
 
 
+
+import Foundation
+
+func solution(_ my_string: String, _ n: Int) -> String {
+
+    let index = my_string.index(my_string.startIndex, offsetBy: n)
+    
+    return String(my_string[..<index])
+}
+
