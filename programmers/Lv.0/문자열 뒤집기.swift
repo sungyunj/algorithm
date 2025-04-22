11890b565c2b3d1d3070e33d3d2f5580e9dadf62
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-04-22 23:04:17 +0900

    feat: algorithm 문제 71 swift로 풀기

--- programmers/Lv.0/문자열 뒤집기.swift
+++ programmers/Lv.0/문자열 뒤집기.swift
@@ -31,3 +31,16 @@
 
 
 
+import Foundation
+
+func solution(_ my_string: String, _ s: Int, _ e: Int) -> String {
+    
+    let startIndex = my_string.index(my_string.startIndex, offsetBy: s)
+    let endIndex = my_string.index(my_string.startIndex, offsetBy: e)
+    
+    let prefix = String(my_string[..<startIndex])
+    let middle = String(my_string[startIndex...endIndex].reversed())
+    let suffix = String(my_string[my_string.index(after: endIndex)...])
+    
+    return prefix + middle + suffix
+}
