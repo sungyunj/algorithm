b9345019ebf075b35d4a403f3791f916cc06d486
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-04-27 14:57:59 +0900

    feat: algorithm 문제 74 풀기

--- programmers/Lv.0/문자 개수 세기.py
+++ programmers/Lv.0/문자 개수 세기.py
@@ -26,3 +26,21 @@
 # 예제 1번의 my_string에서 'P'가 1개, 'a'가 1개, 'e'가 1개, 'g'가 1개, 'm'이 2개, 'o'가 1개, 'r'가 3개, 's'가 1개 있으므로 
 # [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 2, 0, 1, 0, 0, 3, 1, 0, 0, 0, 0, 0, 0, 0]
 # 를 return 합니다.
+
+
+
+def solution(my_string):
+
+    answer = [0] * 52
+
+    for ch in my_string:
+
+        if 'A' <= ch <= 'Z':
+            index = ord(ch) - ord('A')
+        elif 'a' <= ch <= 'z':
+            index = ord(ch) - ord('a') + 26
+        else:
+            continue
+        answer[index] += 1
+        
+    return answer
