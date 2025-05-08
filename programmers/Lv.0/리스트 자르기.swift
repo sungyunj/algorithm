454b9f3d789800f005a1009c08ff9f810cd6510c
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-05-08 20:31:21 +0900

    feat: algorithm 문제 79 swift로 풀기

--- programmers/Lv.0/리스트 자르기.swift
+++ programmers/Lv.0/리스트 자르기.swift
@@ -40,3 +40,32 @@
 
 // 입출력 예 #2
 // [1, 2, 3, 4, 5, 6, 7, 8, 9]에서 1번 인덱스부터 5번 인덱스까지 2개 간격으로 자른 리스트는 [2, 4, 6]입니다.
+
+
+
+
+import Foundation
+
+func solution(_ n: Int, _ slicer: [Int], _ num_list: [Int]) -> [Int] {
+    
+    let a = slicer[0]
+    let b = slicer[1]
+    let c = slicer[2]
+
+    switch n {
+    case 1:
+        return Array(num_list[0...b])
+    case 2:
+        return Array(num_list[a...])
+    case 3:
+        return Array(num_list[a...b])
+    case 4:
+        var result: [Int] = []
+        for i in stride(from: a, through: b, by: c) {
+            result.append(num_list[i])
+        }
+        return result
+    default:
+        return []
+    }
+}
