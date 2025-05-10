d38aa57882111efa350ad2984087ceb00ab52bd9
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-05-10 22:54:54 +0900

    feat: algorithm 문제 80 swift로 풀기

--- programmers/Lv.0/첫 번째로 나오는 음수.swift
+++ programmers/Lv.0/첫 번째로 나오는 음수.swift
@@ -30,3 +30,17 @@
 
 
 
+
+import Foundation
+
+func solution(_ num_list: [Int]) -> Int {
+
+    for (index, num) in num_list.enumerated() {
+
+        if num < 0 {
+            return index
+        }
+    }
+    
+    return -1
+}
