d1d9846b2da1ca246a47b8f4f3da2130497a52dc
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-05-12 21:19:46 +0900

    feat: algorithm 문제 81 swift로 풀기

--- programmers/Lv.0/배열 만들기3.swift
+++ programmers/Lv.0/배열 만들기3.swift
@@ -33,3 +33,13 @@
 
 
 
+
+import Foundation
+
+func solution(_ arr: [Int], _ intervals: [[Int]]) -> [Int] {
+    
+    let first = Array(arr[intervals[0][0]...intervals[0][1]])
+    let second = Array(arr[intervals[1][0]...intervals[1][1]])
+    
+    return first + second
+}
