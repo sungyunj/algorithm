adae937baf235fe75e3d04c42092eacac6cbe840
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-05-21 21:25:48 +0900

    feat: algorithm 문제 86 풀기

--- programmers/Lv.0/왼쪽 오른쪽.py
+++ programmers/Lv.0/왼쪽 오른쪽.py
@@ -33,3 +33,16 @@
 
 
 
+def solution(str_list):
+
+    for i, s in enumerate(str_list):
+        if s == "l":
+            return str_list[:i]
+        elif s == "r":
+            return str_list[i+1:]
+        
+    return []
+
+
+
+# print(solution(["u", "u", "l", "r"]))
