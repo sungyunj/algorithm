8a77d0a3825c5458727dc0f61d5ee7f0ea3804cf
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-05-28 22:28:41 +0900

    feat: algorithm 문제 89 swift로 풀기

--- programmers/Lv.0/홀수vs짝수.swift
+++ programmers/Lv.0/홀수vs짝수.swift
@@ -31,3 +31,21 @@
 
 
 
+
+import Foundation
+
+func solution(_ num_list: [Int]) -> Int {
+    var oddSum = 0
+    var evenSum = 0
+
+    for (index, value) in num_list.enumerated() {
+        if index % 2 == 0 {
+            oddSum += value
+        }
+        else {
+            evenSum += value
+        }
+    }
+
+    return max(oddSum, evenSum)
+}
