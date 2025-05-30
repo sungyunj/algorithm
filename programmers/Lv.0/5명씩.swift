b2cf077fd11dc6de1a65dd27ed35b942ba123b4f
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-05-30 11:23:32 +0900

    feat: algorithm 문제 90 swift로 풀기

--- programmers/Lv.0/5명씩.swift
+++ programmers/Lv.0/5명씩.swift
@@ -26,3 +26,18 @@
 // 입출력 예 #1
 // 앞에서부터 5명씩 두 그룹으로 묶을 수 있습니다. 
 // ["nami", "ahri", "jayce", "garen", "ivern"], ["vex", "jinx"] 이 두 그룹에서 가장 앞에 서있는 사람들의 이름을 담은 리스트인 ["nami", "vex"]를 return합니다.
+
+
+
+
+import Foundation
+
+func solution(_ names: [String]) -> [String] {
+    var answer: [String] = []
+
+    for i in stride(from: 0, to: names.count, by: 5) {
+        answer.append(names[i])
+    }
+    
+    return answer
+}
