0fddba55b4787a225978abfb190d529e8e603bb1
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-06-22 23:27:55 +0900

    feat: algorithm 문제 101 풀기

--- programmers/Lv.0/배열에서 문자열 대소문자 변환하기.py
+++ programmers/Lv.0/배열에서 문자열 대소문자 변환하기.py
@@ -32,3 +32,21 @@
 # strArr[0]은 짝수번째 인덱스의 문자열이므로 소문자로 바꿔서 "abc"가 됩니다.
 # strArr[1]은 홀수번째 인덱스의 문자열이므로 대문자로 바꿔서 "ABC"가 됩니다.
 # 따라서 ["abc","ABC"]를 return 합니다.
+
+
+
+
+def solution(strArr):
+    answer = []
+
+    for i in range(len(strArr)):
+        if i % 2 == 0:
+            answer.append(strArr[i].lower())
+        else:
+            answer.append(strArr[i].upper())
+
+    return answer
+
+
+# print(solution(["AAA","BBB","CCC","DDD"]))
+# print(solution(["aBc","AbC"]))
