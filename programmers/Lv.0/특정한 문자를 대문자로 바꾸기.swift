766bbcbbca61a62b466c5087976f87dc9129e93c
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-06-27 20:23:52 +0900

    feat: algorithm 문제 103 swift로 풀기

--- programmers/Lv.0/특정한 문자를 대문자로 바꾸기.swift
+++ programmers/Lv.0/특정한 문자를 대문자로 바꾸기.swift
@@ -29,3 +29,8 @@
 
 
 
+import Foundation
+
+func solution(_ my_string: String, _ alp: String) -> String {
+    return my_string.map { String($0) == alp ? alp.uppercased() : String($0) }.joined()
+}
