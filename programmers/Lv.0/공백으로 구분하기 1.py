94be36a967b9b4d07ca90de2975548c19819183a
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-07-04 23:18:23 +0900

    feat: algorithm 문제 107 풀기

--- programmers/Lv.0/공백으로 구분하기 1.py
+++ programmers/Lv.0/공백으로 구분하기 1.py
@@ -26,3 +26,13 @@
 
 # 입출력 예 #2
 # 예제 2번의 my_string은 "programmers"로 단어가 하나만 있습니다. 따라서 ["programmers"]를 return 합니다.
+
+
+
+def solution(my_string):
+    return my_string.split()
+
+
+
+# print(solution("i love you"))
+# print(solution("programmers"))
