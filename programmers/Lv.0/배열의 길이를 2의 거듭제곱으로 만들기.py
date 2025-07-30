687655002f9768c65db417c4d47d50467f15876a
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-07-30 14:14:08 +0900

    feat: algorithm 문제 119 풀기

--- programmers/Lv.0/배열의 길이를 2의 거듭제곱으로 만들기.py
+++ programmers/Lv.0/배열의 길이를 2의 거듭제곱으로 만들기.py
@@ -32,3 +32,17 @@
 
 # 예제 2번의 arr의 길이는 4이고 이미 2의 정수 거듭제곱입니다. 
 # 따라서 뒤에 0을 추가하지 않아도 되므로 [58, 172, 746, 89]를 return 합니다.
+
+
+
+
+
+def solution(arr):
+    n = len(arr)
+    power = 1
+
+    while power < n:
+        power *= 2
+
+    zeros_to_add = power - n
+    return arr + [0] * zeros_to_add
