b474851da23a0f420418dbdc0f0049c2940f70b1
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-08-02 23:10:40 +0900

    feat: algorithm 문제 120 swift로 풀기

--- programmers/Lv.0/배열 비교하기.swift
+++ programmers/Lv.0/배열 비교하기.swift
@@ -44,3 +44,27 @@
 
 
 
+import Foundation
+
+func solution(_ arr1: [Int], _ arr2: [Int]) -> Int {
+    if arr1.count > arr2.count {
+        return 1
+    } 
+    else if arr1.count < arr2.count {
+        return -1
+    } 
+    else {
+        let sum1 = arr1.reduce(0, +)
+        let sum2 = arr2.reduce(0, +)
+        
+        if sum1 > sum2 {
+            return 1
+        } 
+        else if sum1 < sum2 {
+            return -1
+        } 
+        else {
+            return 0
+        }
+    }
+}
