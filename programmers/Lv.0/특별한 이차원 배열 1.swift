8faa1e9faa76cd492992d7486320a9ca9070d8a0
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-09-12 21:53:01 +0900

    feat: algorithm 문제 140 swift로 풀기

--- programmers/Lv.0/특별한 이차원 배열 1.swift
+++ programmers/Lv.0/특별한 이차원 배열 1.swift
@@ -51,3 +51,19 @@
 // 0	        1
 
 // 따라서 [[1]]을 return 합니다.
+
+
+
+import Foundation
+
+func solution(_ n: Int) -> [[Int]] {
+    var result = [[Int]]()
+    for i in 0..<n {
+        var row = [Int]()
+        for j in 0..<n {
+            row.append(i == j ? 1 : 0)
+        }
+        result.append(row)
+    }
+    return result
+}
