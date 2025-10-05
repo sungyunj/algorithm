0da8fd749afabcf0c9b37e6a33a772960f60e750
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-10-05 20:35:53 +0900

    feat: algorithm 문제 151 swift로 풀기

--- programmers/Lv.0/7의 배수.swift
+++ programmers/Lv.0/7의 배수.swift
@@ -30,3 +30,12 @@
 
 
 
+import Foundation
+
+func solution(_ array: [Int]) -> Int {
+    return array
+        .map { String($0) }
+        .joined()                    
+        .filter { $0 == "7" }         
+        .count                      
+}
