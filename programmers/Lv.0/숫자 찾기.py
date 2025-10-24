f004ff8a1637d35a24e752540ab8e8e892e0f752
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-10-24 20:42:51 +0900

    feat: algorithm 문제 159 풀기

--- programmers/Lv.0/숫자 찾기.py
+++ programmers/Lv.0/숫자 찾기.py
@@ -27,3 +27,14 @@
 
 # 입출력 예 #3
 # 123456에 7은 없으므로 -1을 return 합니다.
+
+
+
+def solution(num, k):
+    num_str = str(num)
+    k_str = str(k)
+
+    if k_str in num_str:
+        return num_str.index(k_str) + 1
+    else:
+        return -1
