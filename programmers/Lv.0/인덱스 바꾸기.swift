713b83a2099e5bc8352c8665c6903f2b82627532
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-11-13 21:28:58 +0900

    feat: algorithm 문제 166 swift로 풀기

--- programmers/Lv.0/인덱스 바꾸기.swift
+++ programmers/Lv.0/인덱스 바꾸기.swift
@@ -25,3 +25,17 @@
 
 // 입출력 예 #2
 // "I love you"의 3번째 인덱스 "o"와 " "(공백)을 바꾸면 "I l veoyou"입니다.
+
+
+
+import Foundation
+
+func solution(_ my_string: String, _ num1: Int, _ num2: Int) -> String {
+    var chars = Array(my_string)
+
+    let temp = chars[num1]
+    chars[num1] = chars[num2]
+    chars[num2] = temp
+
+    return String(chars)
+}
