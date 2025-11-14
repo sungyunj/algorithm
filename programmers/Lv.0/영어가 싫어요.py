7959a5f48fa3b23504b4105d36fb42f62fda5c3e
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-11-14 23:46:13 +0900

    feat: algorithm 문제 167 풀기

--- programmers/Lv.0/영어가 싫어요.py
+++ programmers/Lv.0/영어가 싫어요.py
@@ -15,7 +15,7 @@
 # 입출력 예
 # numbers	                                result
 # "onetwothreefourfivesixseveneightnine"	123456789
-    # "onefourzerosixseven"	                14067
+# "onefourzerosixseven"	                    14067
 
 
 
@@ -25,3 +25,30 @@
 
 # 입출력 예 #1
 # "onefourzerosixseven"를 숫자로 바꾼 14067를 return합니다.
+
+
+
+def solution(numbers):
+    num_map = {
+        "zero": "0",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+    }
+    
+    result = ""
+    temp = ""
+
+    for ch in numbers:
+        temp += ch
+        if temp in num_map:
+            result += num_map[temp]
+            temp = ""
+    
+    return int(result)
