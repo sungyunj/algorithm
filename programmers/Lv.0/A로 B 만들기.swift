3f974eac2eb3e29e249842df4c9fa351f5846754
성윤 <145305570+sungyunj@users.noreply.github.com>
2025-12-01 15:37:17 +0900

    feat: algorithm 문제 175 swift로 풀기

--- programmers/Lv.0/A로 B 만들기.swift
+++ programmers/Lv.0/A로 B 만들기.swift
@@ -26,3 +26,11 @@
 
 
 
+import Foundation
+
+func solution(_ before: String, _ after: String) -> Int {
+    let sortedBefore = before.sorted()
+    let sortedAfter = after.sorted()
+    
+    return sortedBefore == sortedAfter ? 1 : 0
+}
