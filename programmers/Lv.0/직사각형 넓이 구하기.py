9ad9a233ba873e1afce7c5e880bd36fe09996c3a
성윤 <145305570+sungyunj@users.noreply.github.com>
2026-01-05 22:24:35 +0900

    feat: algorithm 문제 192 풀기

--- programmers/Lv.0/직사각형 넓이 구하기.py
+++ programmers/Lv.0/직사각형 넓이 구하기.py
@@ -25,3 +25,14 @@
 
 # 입출력 예 #2
 # 좌표 [[-1, -1], [1, 1], [1, -1], [-1, 1]]를 꼭짓점으로 갖는 직사각형의 가로, 세로 길이는 각각 2, 2이므로 직사각형의 넓이는 2 x 2 = 4입니다.
+
+
+
+def solution(dots):
+    x_coords = [dot[0] for dot in dots]
+    y_coords = [dot[1] for dot in dots]
+
+    width = max(x_coords) - min(x_coords)
+    height = max(y_coords) - min(y_coords)
+
+    return width * height
