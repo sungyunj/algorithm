3d8e7df9c9b1302606777d5d994909597df237ac
성윤 <145305570+sungyunj@users.noreply.github.com>
2026-01-24 23:43:55 +0900

    feat: algorithm 문제 201 풀기

--- programmers/Lv.0/합성수 찾기.py
+++ programmers/Lv.0/합성수 찾기.py
@@ -20,3 +20,18 @@
 # 입출력 예 #1
 
 # 15 이하 합성수는 4, 6, 8, 9, 10, 12, 14, 15 로 8개입니다. 따라서 8을 return합니다.
+
+
+
+def solution(n):
+    answer = 0
+    
+    for i in range(4, n + 1):
+        divisor_count = 0
+        for j in range(1, i + 1):
+            if i % j == 0:
+                divisor_count += 1
+        if divisor_count >= 3:
+            answer += 1
+            
+    return answer
