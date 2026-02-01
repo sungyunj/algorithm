d16719d26cecd61212e2ba294d1f380843bebb05
성윤 <145305570+sungyunj@users.noreply.github.com>
2026-02-01 22:47:54 +0900

    feat: algorithm 문제 204 풀기

--- programmers/Lv.0/공 던지기.py
+++ programmers/Lv.0/공 던지기.py
@@ -37,3 +37,10 @@
 # 1번은 첫 번째로 3번에게 공을 던집니다.
 # 3번은 두 번째로 2번에게 공을 던집니다.
 # 2번은 세 번째로 1번에게 공을 던집니다.
+
+
+
+def solution(numbers, k):
+    idx = (2 * (k - 1)) % len(numbers)
+    
+    return numbers[idx]
