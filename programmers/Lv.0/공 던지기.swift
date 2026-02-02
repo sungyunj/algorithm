cdd53e85939954bad52046528a06e6b2f1f3eb79
성윤 <145305570+sungyunj@users.noreply.github.com>
2026-02-02 20:50:02 +0900

    feat: algorithm 문제 204 swift로 풀기

--- programmers/Lv.0/공 던지기.swift
+++ programmers/Lv.0/공 던지기.swift
@@ -37,3 +37,13 @@
 // 1번은 첫 번째로 3번에게 공을 던집니다.
 // 3번은 두 번째로 2번에게 공을 던집니다.
 // 2번은 세 번째로 1번에게 공을 던집니다.
+
+
+
+import Foundation
+
+func solution(_ numbers: [Int], _ k: Int) -> Int {
+    let index = (2 * (k - 1)) % numbers.count
+    
+    return numbers[index]
+}
