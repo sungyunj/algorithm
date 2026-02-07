1d801e9d120cff01fedbb56e5af64e62f7317495
성윤 <145305570+sungyunj@users.noreply.github.com>
2026-02-07 23:46:39 +0900

    feat: algorithm 문제 206 swift로 풀기

--- programmers/Lv.0/점의 위치 구하기.swift
+++ programmers/Lv.0/점의 위치 구하기.swift
@@ -31,3 +31,26 @@
 
 // 입출력 예 #2
 // dot이 [-7, 9]로 x 좌표가 음수, y 좌표가 양수이므로 제 2 사분면에 속합니다. 따라서 2를 return 합니다.
+
+
+
+
+import Foundation
+
+func solution(_ dot: [Int]) -> Int {
+    let x = dot[0]
+    let y = dot[1]
+    
+    if x > 0 && y > 0 {
+        return 1
+    } 
+    else if x < 0 && y > 0 {
+        return 2
+    } 
+    else if x < 0 && y < 0 {
+        return 3
+    } 
+    else {
+        return 4
+    }
+}
