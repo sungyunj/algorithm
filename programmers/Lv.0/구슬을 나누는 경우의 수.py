a3414dde9dd4f426a1144ca6a08a3940498e170c
성윤 <145305570+sungyunj@users.noreply.github.com>
2026-02-08 18:58:52 +0900

    feat: algorithm 문제 207 풀기

--- programmers/Lv.0/구슬을 나누는 경우의 수.py
+++ programmers/Lv.0/구슬을 나누는 경우의 수.py
@@ -32,3 +32,12 @@
 # Hint
 # 서로 다른 n개 중 m개를 뽑는 경우의 수 공식은 다음과 같습니다.
 # n! / (n-m)! * m! 
+
+
+
+def solution(balls, share):
+    result = 1
+    for i in range(1, share + 1):
+        result = result * (balls - i + 1) // i
+        
+    return result
