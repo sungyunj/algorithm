8a174846c5ca67371853054537538355846d0075
성윤 <145305570+sungyunj@users.noreply.github.com>
2026-03-24 15:23:45 +0900

    feat: algorithm 문제 225 swift로 풀기

--- programmers/Lv.0/아이스 아메리카노.swift
+++ programmers/Lv.0/아이스 아메리카노.swift
@@ -23,3 +23,13 @@
 
 // 입출력 예 #2
 // 15,000원은 아이스 아메리카노 두 잔을 살 수 있고 잔돈은 4,000원입니다.
+
+
+
+import Foundation
+
+func solution(_ money: Int) -> [Int] {
+    let cups = money / 5500
+    let change = money % 5500
+    return [cups, change]
+}
