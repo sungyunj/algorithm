35f2f55ae8a3b29304a447686b54d1b0a4d6043d
성윤 <145305570+sungyunj@users.noreply.github.com>
2026-03-25 21:11:16 +0900

    feat: algorithm 문제 226 풀기

--- programmers/Lv.0/옷가게 할인 받기.py
+++ programmers/Lv.0/옷가게 할인 받기.py
@@ -24,3 +24,15 @@
 
 # 입출력 예 #2
 # 580,000원에서 20%를 할인한 464,000원을 return 합니다.
+
+
+
+def solution(price):
+    if price >= 500000:
+        return int(price * 0.8)
+    elif price >= 300000:
+        return int(price * 0.9)
+    elif price >= 100000:
+        return int(price * 0.95)
+    else:
+        return price
