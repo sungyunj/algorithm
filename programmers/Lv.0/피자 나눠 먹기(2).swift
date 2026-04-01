47d864d655b43c5e87c7ee79f36894d006416776
성윤 <145305570+sungyunj@users.noreply.github.com>
2026-04-01 20:18:40 +0900

    feat: algorithm 문제 229 swift로 풀기

--- programmers/Lv.0/피자 나눠 먹기(2).swift
+++ programmers/Lv.0/피자 나눠 먹기(2).swift
@@ -27,3 +27,15 @@
 
 // 입출력 예 #3
 // 4명이 모두 같은 양을 먹기 위해 최소 2판을 시키면 피자가 12조각으로 모두 세 조각씩 먹을 수 있습니다.
+
+
+
+import Foundation
+
+func solution(_ n: Int) -> Int {
+    var i = 1
+    while (6 * i) % n != 0 {
+        i += 1
+    }
+    return i
+}
