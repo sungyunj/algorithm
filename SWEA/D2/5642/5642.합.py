05be2e2c71d546a819c9ac5eb645401a5ecb1556
성윤 <145305570+sungyunj@users.noreply.github.com>
2026-05-26 22:08:58 +0900

    feat: 5642 문제 풀이

--- SWEA/D2/5642/5642.합.py
+++ SWEA/D2/5642/5642.합.py
@@ -13,3 +13,28 @@
 
 # [출력]
 # 각 테스트케이스마다 한 줄에 걸쳐, 테스트케이스 수 “#(TC) “를 출력하고, 연속된 정수의 합의 최대값을 출력하시오.
+
+
+import sys
+sys.stdin = open("/Users/tjddbsj/Desktop/github/algorithm/algorithm/SWEA/D2/5642/sample_input.txt","r")
+
+T = int(input())
+
+for tc in range(1, T + 1):
+    N = int(input())
+    A = list(map(int,input().split()))
+    # 각 단계에서 이전까지의 합에 현재 숫자를 더하는 것이 이득인가
+    # 아니면 여기서부터 새로 시작하는 것이 이득인가를 결정
+    max_sum = A[0]
+    cur_sum = A[0]
+
+    for i in range(1, N):
+        # (이전까지의 합 + 현재 값) vs (현재 값) 중 큰 것을 선택
+        # current_sum + nums[i]가 nums[i]보다 작다면 
+        # 앞선 합이 마이너스라는 뜻. 새로 시작하는 게 이득
+        cur_sum = max(cur_sum + A[i], A[i])
+
+        if cur_sum > max_sum:
+            max_sum = cur_sum
+    
+    print(f"#{tc} {max_sum}")
