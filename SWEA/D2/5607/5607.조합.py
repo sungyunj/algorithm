f0b5bcb5c100428610f4379e1fd30f8324684e1c
성윤 <145305570+sungyunj@users.noreply.github.com>
2026-05-28 23:27:07 +0900

    feat: 5607 풀이

--- SWEA/D2/5607/5607.조합.py
+++ SWEA/D2/5607/5607.조합.py
@@ -13,3 +13,31 @@
 
 
 
+# import sys
+# sys.stdin = open("/Users/tjddbsj/Desktop/github/algorithm/algorithm/SWEA/D2/5607/sample_input.txt","r")
+
+# 팩토리얼 배열 미리 만들기 (최대 범위 1,000,000)
+# 테스트 케이스 밖에서 한 번만 계산해야 시간 초과가 나지 않음.
+MOD = 1234567891
+fact = [1] * 1000001
+for i in range(2, 1000001):
+    fact[i] = (fact[i-1] * i) % MOD
+
+T = int(input())
+
+for tc in range(1, T + 1):
+    N, R = map(int, input().split())
+
+    # 조합 공식: N! / (R! * (N-R)!)
+    # 분자: N!
+    top = fact[N]
+    # 분모: R! * (N-R)!
+    bottom = (fact[R] * fact[N-R]) % MOD
+
+    # 4. 페르마의 소정리 적용
+    # 나누기 bottom 대신, bottom^(MOD-2)를 곱함.
+    # pow(a, b, m)은 (a**b) % m 을 아주 빠르게 계산함.
+    inv_bottom = pow(bottom, MOD - 2, MOD)
+
+    ans = (top * inv_bottom) % MOD
+    print(f"#{tc} {ans}")
