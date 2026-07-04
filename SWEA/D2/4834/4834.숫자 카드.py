58d81c6490b390a40b1ac3709b35479362d07e5e
성윤 <145305570+sungyunj@users.noreply.github.com>
2026-07-04 23:11:24 +0900

    feat: 4834 문제 풀이

--- SWEA/D2/4834/4834.숫자 카드.py
+++ SWEA/D2/4834/4834.숫자 카드.py
@@ -11,3 +11,32 @@
 # [출력]
 # 각 줄마다 "#T" (T는 테스트 케이스 번호)를 출력한 뒤, 가장 많은 카드의 숫자와 장 수를 차례로 출력한다.
 
+
+import sys
+sys.stdin = open("/Users/tjddbsj/Desktop/github/algorithm/algorithm/SWEA/D2/4834/sample_input.txt","r")
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    N = int(input())
+    cards = input()
+    
+    # 0부터 9까지 숫자의 개수를 저장 카운팅 배열 (크기 10)
+    count = [0] * 10
+    
+    # 각 숫자의 등장 횟수 세기
+    for card in cards:
+        count[int(card)] += 1
+        
+    max_card = 0  # 가장 많은 카드의 숫자
+    max_count = 0 # 가장 많은 카드의 장수
+    
+    # 0부터 9까지 확인하며 최댓값 갱신
+    # 장수가 같을 때는 숫자가 큰 쪽을 선택해야 하므로, 
+    # 부등호를 '>'가 아닌 '>='로 설정하여 같은 장수일 때 더 큰 숫자로 갱신되도록 함.
+    for i in range(10):
+        if count[i] >= max_count:
+            max_count = count[i]
+            max_card = i
+            
+    print(f"#{test_case} {max_card} {max_count}")
