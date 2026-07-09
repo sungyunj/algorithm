80a729865f54c807645ce5376bfb313cf6e1163b
성윤 <145305570+sungyunj@users.noreply.github.com>
2026-07-09 22:25:22 +0900

    feat: 26009 문제 풀이

--- SWEA/D3/26009/26009.곱의 합.py
+++ SWEA/D3/26009/26009.곱의 합.py
@@ -12,3 +12,40 @@
 # [출력]
 # 각 테스트 케이스 마다 한 줄씩, 정답을 출력하라.
 
+
+
+import sys
+sys.stdin = open("/Users/tjddbsj/Desktop/github/algorithm/algorithm/SWEA/D3/26009/1_sample_input.txt","r")
+
+def solve():
+    tc = int(input())
+    
+    MOD = 998244353
+    
+    # 1부터 N까지의 합을 MOD 연산하며 구하는 함수
+    def get_sum(n):
+        # n과 n+1 중 하나는 반드시 짝수이므로, 먼저 2로 나누어 소수점 발생을 방지
+        if n % 2 == 0:
+            term1 = (n // 2) % MOD
+            term2 = (n + 1) % MOD
+        else:
+            term1 = n % MOD
+            term2 = ((n + 1) // 2) % MOD
+        return (term1 * term2) % MOD
+
+    for _ in range(tc):
+        a, b, c = map(int, input().split())
+        
+        # 각각의 합을 구함
+        sum_a = get_sum(a)
+        sum_b = get_sum(b)
+        sum_c = get_sum(c)
+        
+        # 최종 곱의 합 구하기 (중간중간 MOD 연산)
+        ans = (sum_a * sum_b) % MOD
+        ans = (ans * sum_c) % MOD
+
+        print(ans)
+
+if __name__ == '__main__':
+    solve()
